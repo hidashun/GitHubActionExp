f77906d9b3749f22efcb1824340257d328e6e100
Shunsuke Hida <kizai07@gmail.com>
2023-02-26 17:04:05 +0900

    Initial Commit

---
+++ GitHubActionExp/GitHubActionExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActionExpApp.swift
+//  GitHubActionExp
+//
+//  Created by Shunsuke Hida on 2023/02/26.
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
